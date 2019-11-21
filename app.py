2bfdfcc78b7314a960629628f198311e059b4287
Zeheng Li <imzehengl@gmail.com>
2019-11-21 12:25:16 -0700

    refresh

--- app.py
+++ app.py
@@ -1,23 +1,34 @@
+import os
+
 from joblib import load
 from flask import Flask, render_template, request
+from flask_bootstrap import Bootstrap
 from whitenoise import WhiteNoise
 
+from forms import IngredientForm
+
+
 app = Flask(__name__)
+Bootstrap(app)
 app.wsgi_app = WhiteNoise(app.wsgi_app, root="static/")
+app.config["SECRET_KEY"] = os.getenv("SECRET_KEY", "SECRET_KEY")
+app.config["BOOTSTRAP_SERVE_LOCAL"] = True
 model = load("model_sklearn")
 
 
-@app.route("/", methods=["GET", "POST"])
+def get_cuisine(ingredients):
+    if not ingredients:
+        return None
+
+    return model.predict([ingredients]).item()
+
+
+@app.route("/", methods=["get", "post"])
 def index():
-    if request.method == "POST":
-        ingredients = request.form["ingredients"]
-        return render_template(
-            "index.html",
-            cuisine=model.predict([ingredients]).item(),
-            ingredients=ingredients,
-        )
-    else:
-        return render_template("index.html")
+    form = IngredientForm(request.form)
+    cuisine = get_cuisine(form.ingredients.data)
+
+    return render_template("index.html", form=form, cuisine=cuisine)
 
 
 if __name__ == "__main__":
